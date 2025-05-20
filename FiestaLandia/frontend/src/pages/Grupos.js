e617bb5bf72e043aecccc631a7b7d6a1aeeb258a
Mildred <milsugei71@gmail.com>
2025-05-20 08:54:55 -0600

    ya tiene imagnes de los grupos

--- FiestaLandia/frontend/src/pages/Grupos.js
+++ FiestaLandia/frontend/src/pages/Grupos.js
@@ -1,29 +1,79 @@
 // src/pages/Grupos.js
 import React from 'react';
+import '../styles/style.css';
 
-//import '../styles/Grupos.css';
+import soniderosImg from '../assets/sonideros.jpg';
+import mariachiImg from '../assets/mariachi.jpg';
+import karabaliImg from '../assets/karabali.jpg';
+import nelsonImg from '../assets/Nelson.jpg';
+import juniorKlanImg from '../assets/juniorklan.jpg';
+import sociosImg from '../assets/socios.jpg';
+
+
 
 
 const gruposMusicales = [
   {
     nombre: 'Los Sonideros',
-    genero: 'Cumbia',
+    genero: 'Sonidero',
     trayectoria: '10 años',
     musicos: 6,
     costos: 'Desde $8,000 MXN',
     equipo: 'Bocinas JBL, luces robóticas, escenario móvil',
     extra: '$1,000 MXN por hora',
+    imagen: soniderosImg,
   },
   {
     nombre: 'Mariachi Real',
-    genero: 'Ranchero',
+    genero: 'Ranchero, Bolero etc',
     trayectoria: '15 años',
     musicos: 8,
     costos: 'Desde $6,500 MXN',
     equipo: 'Micrófonos inalámbricos, trajes típicos',
     extra: '$900 MXN por hora',
+    imagen: mariachiImg,
   },
   // Puedes agregar más grupos aquí...
+  {
+    nombre: 'Super Grupo Karabali',
+    genero: 'Cumbia, Electrocumbia',
+    trayectoria: '12 años',
+    musicos: 8,
+    costos: 'Desde $10,500 MXN',
+    equipo: 'Bocinas (medias, graves, aereas), luces de ambiente',
+    extra: '$1000 MXN por hora',
+    imagen: karabaliImg,
+  },
+  {
+    nombre: 'Nelson Kansela',
+    genero: 'Cumbia Tropical',
+    trayectoria: '30 años',
+    musicos: 5,
+    costos: 'Desde $180,000 MXN',
+    equipo: 'Equipo Grande para bailes',
+    extra: '$20,000 MXN por hora',
+    imagen: nelsonImg,
+  },
+  {
+    nombre: 'Junior Klan',
+    genero: 'Cumbia Tropical',
+    trayectoria: '40 años',
+    musicos: 14,
+    costos: 'Desde $250,000 MXN',
+    equipo: 'Equipo Grande para bailes',
+    extra: '$50,000 MXN por hora',
+    imagen: juniorKlanImg,
+  },
+  {
+    nombre: 'Socios del Ritmo',
+    genero: 'Cumbia Tropical',
+    trayectoria: '60 años',
+    musicos: 11,
+    costos: 'Desde $150,000 MXN',
+    equipo: 'Equipo Grande para bailes',
+    extra: '$20,000 MXN por hora',
+    imagen: sociosImg,
+  },
 ];
 
 const Grupos = () => {
@@ -33,6 +83,7 @@ const Grupos = () => {
       <div className="cards">
         {gruposMusicales.map((grupo, index) => (
           <div className="grupo-card" key={index}>
+            <img src={grupo.imagen} alt={grupo.nombre} className="grupo-imagen" />
             <h2>{grupo.nombre}</h2>
             <h3>Género: {grupo.genero}</h3>
             <p><strong>Años de trayectoria:</strong> {grupo.trayectoria}</p>
