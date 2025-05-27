da1dd4c53290a7614ed8d1479d1880da3a63554a
MelanieAnzures <melanieanzures4@gmail.com>
2025-05-26 23:30:06 -0600

    Commit git pull

--- FiestaLandia/backend/backend/settings.py
+++ FiestaLandia/backend/backend/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'fiestalandia',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': '12345678',
         'HOST': 'localhost',
         'PORT': '3306',
     }
