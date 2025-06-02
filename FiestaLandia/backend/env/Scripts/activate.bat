f447073ecd2fa67c8ea4b1f91a57ef4c0d488426
JcarloRG <227O00510@itsx.edu.mx>
2025-06-02 09:57:19 -0600

    Proyecto Django para Render

--- FiestaLandia/backend/env/Scripts/activate.bat
+++ FiestaLandia/backend/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\abica\OneDrive\Documentos\6_Semestre\Programacion_web\FiestaLandia\fiestalandia\backend\env"
+set "VIRTUAL_ENV=C:\Users\charl\Proyectofinal\FiestaLandia\FiestaLandia\backend\env"
 
 if not defined PROMPT set PROMPT=$P$G
 
